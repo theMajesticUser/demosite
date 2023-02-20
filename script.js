ef67f3108e73a79ef2577255f6b24542630f0ca3
Azeem Parvez <azeemparvezfb@gmail.com>
2023-02-20 10:30:45 +0530

    Initial

---
+++ script.js
@@ -0,0 +1,8 @@
+function mbldropdown() {
+  var x = document.getElementById("dropdown_menu");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
